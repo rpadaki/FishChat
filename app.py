74d99d6c14a85d643a3606f2cc3e83eef66ba9ad
Roshan Padaki <roshanpadaki@gmail.com>
2018-04-20 01:35:27 -0400

    blah

--- app.py
+++ app.py
@@ -196,4 +196,5 @@ def get_hand():
     return jsonify(response), 200
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
