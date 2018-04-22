0639a431f2e2e2b553aab8ae8815c27ddf65c886
Roshan Padaki <roshanpadaki@gmail.com>
2018-04-21 20:02:08 -0400

    blahblah

--- app.py
+++ app.py
@@ -209,4 +209,4 @@ def get_hand():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
